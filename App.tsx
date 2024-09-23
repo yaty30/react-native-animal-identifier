85a49e394424193f538042b61a69e9c4c2cb1248
James <josueyat318@gmail.com>
2024-09-23 23:05:22 +0800

    separated Home from App

--- App.tsx
+++ App.tsx
@@ -6,66 +6,7 @@ import { observer } from 'mobx-react-lite';
 import { globalVariables, objects } from './stores/store';
 import { MainLoadingView, MainStyles } from './stores/styles';
 
-import Camera from './components/Camera';
-import Comment from './components/Comment';
-import InputArea from './components/InputArea';
-import DescriptionCard from './components/DescriptionCard';
-
-
-const MainView = observer(() => {
-    const handleContainerPress = () => {
-        Keyboard.dismiss();
-    };
-
-    return (
-        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <TouchableWithoutFeedback onPress={handleContainerPress}>
-                <View style={MainStyles.container}>
-                    <Camera />
-                    {
-                        !globalVariables.keyboardTrigger &&
-                        objects.data.map((object, index) =>
-                            <DescriptionCard
-                                key={index}
-                                x={object.x}
-                                y={object.y}
-                                width={object.width}
-                                height={object.height}
-                                object={object.object}
-                                confident={object.confident}
-                            />
-                        )
-                    }
-                    <View style={MainStyles.bodyView}>
-                        <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', width: '100%' }}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <TouchableOpacity style={MainStyles.avatar}></TouchableOpacity>
-                                <Text style={MainStyles.name}>Name</Text>
-                            </View>
-                            <TouchableOpacity
-                                style={[
-                                    MainStyles.status,
-                                    {
-                                        backgroundColor: globalVariables.recording ? 'rgba(241, 26, 67, 0.6)' : 'rgba(149, 20, 232, 0.6)',
-                                    }
-                                ]}
-                            >
-                                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#f4f4f4' }}>
-                                    {globalVariables.recording ? "Recording" : "Waiting"}
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-
-                        <Comment />
-                        <InputArea />
-
-                    </View>
-                </View>
-            </TouchableWithoutFeedback>
-
-        </KeyboardAvoidingView>
-    );
-})
+import Home from './components/Home';
 
 export default observer(() => {
     return (
@@ -77,6 +18,6 @@ export default observer(() => {
                 </Text>
             </View>
             :
-            <MainView />
+            <Home />
     )
 })
---
+++ components/Home.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+import { observer } from 'mobx-react-lite';
+import { globalVariables, objects } from '../stores/store';
+import { MainStyles } from '../stores/styles';
+
+import Camera from '../components/Camera';
+import Comment from '../components/Comment';
+import InputArea from '../components/InputArea';
+import DescriptionCard from '../components/DescriptionCard';
+
+
+export default observer(() => {
+    const handleContainerPress = () => {
+        Keyboard.dismiss();
+    };
+
+    return (
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <TouchableWithoutFeedback onPress={handleContainerPress}>
+                <View style={MainStyles.container}>
+                    <Camera />
+                    {
+                        !globalVariables.keyboardTrigger &&
+                        objects.data.map((object, index) =>
+                            <DescriptionCard
+                                key={index}
+                                x={object.x}
+                                y={object.y}
+                                width={object.width}
+                                height={object.height}
+                                object={object.object}
+                                confident={object.confident}
+                            />
+                        )
+                    }
+                    <View style={MainStyles.bodyView}>
+                        <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', width: '100%' }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <TouchableOpacity style={MainStyles.avatar}></TouchableOpacity>
+                                <Text style={MainStyles.name}>Name</Text>
+                            </View>
+                            <TouchableOpacity
+                                style={[
+                                    MainStyles.status,
+                                    {
+                                        backgroundColor: globalVariables.recording ? 'rgba(241, 26, 67, 0.6)' : 'rgba(149, 20, 232, 0.6)',
+                                    }
+                                ]}
+                            >
+                                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#f4f4f4' }}>
+                                    {globalVariables.recording ? "Recording" : "Waiting"}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Comment />
+                        <InputArea />
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    );
+})
