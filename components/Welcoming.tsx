131414494dac313c49d60cc5ec452de918e02a3d
James <josueyat318@gmail.com>
2024-09-27 18:09:26 +0800

    Separating functions from Welcoming to utils

--- components/Welcoming.tsx
+++ components/Welcoming.tsx
@@ -1,6 +1,5 @@
 import React, { useState, memo } from 'react';
-import { Image, Keyboard, Text, TextInput, TouchableOpacity } from 'react-native';
-import { View, Animated, StyleSheet } from 'react-native';
+import { Image, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { observer } from 'mobx-react-lite';
 
 import AnimatedAnimals from './AnimatedAnimals';
@@ -10,75 +9,15 @@ import { globalVariables } from '../stores/store';
 import { WelcomeView } from '../stores/styles';
 import Feather from 'react-native-vector-icons/Feather';
 import { talk } from '../api/api';
-
-const allIcons = [
-    require('../assets/animal_icons/1.png'),
-    require('../assets/animal_icons/2.png'),
-    require('../assets/animal_icons/3.png'),
-    require('../assets/animal_icons/4.png'),
-    require('../assets/animal_icons/5.png'),
-    require('../assets/animal_icons/6.png'),
-    require('../assets/animal_icons/7.png'),
-    require('../assets/animal_icons/8.png'),
-    require('../assets/animal_icons/9.png'),
-    require('../assets/animal_icons/10.png'),
-    require('../assets/animal_icons/11.png'),
-    require('../assets/animal_icons/12.png'),
-    require('../assets/animal_icons/13.png'),
-    require('../assets/animal_icons/14.png'),
-    require('../assets/animal_icons/15.png'),
-    require('../assets/animal_icons/16.png'),
-    require('../assets/animal_icons/17.png'),
-    require('../assets/animal_icons/18.png'),
-    require('../assets/animal_icons/19.png'),
-    require('../assets/animal_icons/20.png'),
-    require('../assets/animal_icons/21.png'),
-    require('../assets/animal_icons/22.png'),
-    require('../assets/animal_icons/23.png'),
-    require('../assets/animal_icons/24.png')
-]
+import { getRandomValues, shuffleArray, WelcomingIcons } from '../stores/utils';
+import Initialising from './Initialising';
 
 const getRandomIconsList = () => {
-    const shuffled = allIcons.sort(() => 0.5 - Math.random());
+    const shuffled = WelcomingIcons.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, 15);
 }
 
 const paths = getRandomIconsList()
-
-const randomDuration = () => {
-    const min = 7500;
-    const max = 14500;
-    const step = 500;
-
-    const numSteps = (max - min) / step;
-    const randomStepIndex = Math.floor(Math.random() * (numSteps + 1));
-
-    return min + randomStepIndex * step;
-};
-
-const getRandomValues = (latency: number) => {
-    const duration = randomDuration() + latency;
-    const durations = [duration, duration];
-    const startX = Math.floor(Math.random() * (101)) - 200;
-    const endX = Math.floor(Math.random() * (420)) + 600;
-
-    const outputRange = [startX, endX];
-
-
-    return {
-        duration: durations,
-        outputRange: outputRange
-    }
-}
-
-const shuffleArray = (array: any) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-};
 
 interface AnimationProps {
     size: number;
@@ -144,12 +83,7 @@ export default observer(() => {
             )}
             <View style={WelcomeView.inputContainer}>
                 {globalVariables.initialLoading ?
-                    <View>
-                        <ProgressBar color="#9514E8" />
-                        <Text style={WelcomeView.initialiseText}>
-                            Initialising...
-                        </Text>
-                    </View>
+                    <Initialising />
                     :
                     <View style={WelcomeView.inputView}>
                         <Image source={require("../assets/welcome.png")} style={WelcomeView.welcome} />
--- stores/utils.tsx
+++ stores/utils.tsx
@@ -2,3 +2,65 @@ export const GetTimestamp = () => {
     const d = new Date()
     return d.getTime();
 }
+
+export const WelcomingIcons = [
+    require('../assets/animal_icons/1.png'),
+    require('../assets/animal_icons/2.png'),
+    require('../assets/animal_icons/3.png'),
+    require('../assets/animal_icons/4.png'),
+    require('../assets/animal_icons/5.png'),
+    require('../assets/animal_icons/6.png'),
+    require('../assets/animal_icons/7.png'),
+    require('../assets/animal_icons/8.png'),
+    require('../assets/animal_icons/9.png'),
+    require('../assets/animal_icons/10.png'),
+    require('../assets/animal_icons/11.png'),
+    require('../assets/animal_icons/12.png'),
+    require('../assets/animal_icons/13.png'),
+    require('../assets/animal_icons/14.png'),
+    require('../assets/animal_icons/15.png'),
+    require('../assets/animal_icons/16.png'),
+    require('../assets/animal_icons/17.png'),
+    require('../assets/animal_icons/18.png'),
+    require('../assets/animal_icons/19.png'),
+    require('../assets/animal_icons/20.png'),
+    require('../assets/animal_icons/21.png'),
+    require('../assets/animal_icons/22.png'),
+    require('../assets/animal_icons/23.png'),
+    require('../assets/animal_icons/24.png')
+]
+
+const WelcomingRandomDuration = () => {
+    const min = 7500;
+    const max = 14500;
+    const step = 500;
+
+    const numSteps = (max - min) / step;
+    const randomStepIndex = Math.floor(Math.random() * (numSteps + 1));
+
+    return min + randomStepIndex * step;
+}
+
+export const getRandomValues = (latency: number) => {
+    const duration = WelcomingRandomDuration() + latency;
+    const durations = [duration, duration];
+    const startX = Math.floor(Math.random() * (101)) - 200;
+    const endX = Math.floor(Math.random() * (420)) + 600;
+
+    const outputRange = [startX, endX];
+
+
+    return {
+        duration: durations,
+        outputRange: outputRange
+    }
+}
+
+export const shuffleArray = (array: any) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
